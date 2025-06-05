633b5ea68952bd1f8919f0fd98c044ed8b82d949
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-06-05 07:58:10 +0200

    'Authorize' annotation added to controller.

--- Presentation/Controllers/ProfilesController.cs
+++ Presentation/Controllers/ProfilesController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Presentation.Models;
 using Presentation.Services;
 
 namespace Presentation.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class ProfilesController(IProfileService profileService) : ControllerBase
